d0a2179a589eca30e6614e2298e04ab59e094f04
admin <kevinkea@163.com>
2018-05-14 08:52:42 +0800

    gym

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"https://easy-mock.com/mock/5acc16c54d607754447ab4d5/gym"',
+
 })
